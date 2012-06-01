08bec20bd91cd31cc7d56c4ba73ca6dd4f704ca3
unknown <jagarbut@jagarbut-pc.(none)>
2012-05-31 21:27:18 -0400

    changes to buildfiles

---
+++ dependencies.rb
@@ -0,0 +1,9 @@
+repositories.remote << "http://repo1.maven.org/maven2"
+
+MOCKITO = 'org.mockito:mockito-all:jar:1.9.0'
+HAMCREST = 'org.hamcrest:hamcrest-all:jar:1.1'
+LOG4J = 'log4j:log4j:jar:1.2.17'
+LOMBOK = 'org.projectlombok:lombok:jar:0.11.0'
+
+DEPENDENCIES = LOG4J, LOMBOK
+TEST_DEPENDENCIES = MOCKITO, HAMCREST
